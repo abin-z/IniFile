ad8a8c80f0b20ad3f7092bfa7674177703abdcb0
abin <abin.z@outlook.com>
2025-02-23 21:49:30 +0800

    init: 初始化仓库

---
+++ include/inifile.h
@@ -0,0 +1,13 @@
+/**
+ * @description: Small inifile parsing library, supports parsing, modifying and saving ini files.
+ * @author: abin
+ * @date: 2025-02-23
+ * @license: MIT
+ */
+
+ namespace ini
+ {
+  
+ } // namespace ini
+
+ 
