45a56902a427fae4a93d7a2336677d3476f08883
abin <abin.z@outlook.com>
2025-03-01 12:11:10 +0800

    feat: 头文件添加MIT协议声明

--- include/inifile/inifile.h
+++ include/inifile/inifile.h
@@ -1,9 +1,41 @@
 /**
- * @description: Small inifile parsing library, supports parsing, modifying and saving ini files.
+ * MIT License
+ * Copyright (c) 2025 Abin
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+
+/**************************************************************************************************************
+ * @description: Easy-to-use ini file parsing library that supports parsing, modifying and saving ini files.
+ * - Easy-to-use INI file parsing library that supports:
+ *   - Reading and writing INI configuration files
+ *   - Section-based key-value storage
+ *   - Automatic type conversion for numeric and boolean values
+ *   - Error handling for malformed INI files
+ *   - Minimal dependencies (only requires C++11 standard library)
+ * 
  * @author: abin
  * @date: 2025-02-23
  * @license: MIT
- */
+ * @repository: https://github.com/abin-z/IniFile
+ **************************************************************************************************************/
 
 #ifndef INI_FILE_H_
 #define INI_FILE_H_
