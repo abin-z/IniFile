54b0f212f32ad1b6b95af5caf3366628b490a790
abin-z <abin.z@outlook.com>
2025-03-10 11:10:24 +0800

    feat: 添加更多的使用案例

---
+++ examples/inifile_basic.cpp
@@ -0,0 +1,68 @@
+#include <inifile/inifile.h>
+
+constexpr char path[] = "basic.ini";
+const char *str = R"(
+  [section]
+  key=value
+)";
+
+void save_func()
+{
+  ini::inifile inif;
+  inif["section"]["key0"] = true;
+  inif["section"]["key1"] = 3.14159;
+  inif["section"]["key2"] = "value";
+  // 调用save方法保存ini文件, 返回是否保存成功
+  bool isok = inif.save(path);
+}
+
+void load_func()
+{
+  ini::inifile inif;
+  // 调用load方法加载ini文件, 返回是否加载成功
+  bool isok = inif.load(path);
+  bool b = inif["section"]["key0"];
+  double d = inif["section"]["key1"];
+  std::string s = inif["section"]["key2"];
+}
+
+void read_func()
+{
+  std::istringstream is(str); // 其他istream也可以, 这里用stringstream演示
+  ini::inifile inif;
+  inif.read(is);
+}
+
+void write_func()
+{
+  std::ostringstream os; // 其他ostream也可以, 这里用stringstream演示
+  ini::inifile inif;
+  inif["section"]["key"] = "value";
+  inif.write(os);
+}
+
+void to_string_func()
+{
+  ini::inifile inif;
+  inif["section"]["key"] = "value";
+  std::string s = inif.to_string();
+}
+
+void from_string_func()
+{
+  std::string s(str);
+  ini::inifile inif;
+  inif.from_string(s);
+}
+
+int main()
+{
+  save_func();
+  load_func();
+
+  read_func();
+  write_func();
+
+  to_string_func();
+  from_string_func();
+}
